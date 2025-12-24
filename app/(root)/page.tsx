286b12c3c898b5c2e044cee8567d85932e7036d0
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-12-24 14:34:48 -0500

    api routes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,14 @@
 import Hello from '../components/hello';
 
-const Home = () => {
-  console.log('Home component rendered');
+const Home = async () => {
+  const response = await fetch('http://localhost:3000/api/books');
+
+  const books = await response.json();
   return (
     <main>
       <div className="text-5xl underline">Welcome to Next.js!</div>;
       <Hello />
+      <code>{JSON.stringify(books, null, 2)}</code>
     </main>
   );
 };
---
+++ app/api/db.ts
@@ -0,0 +1,7 @@
+const books = [
+  { id: 1, name: 'Atomic Habits' },
+  { id: 2, name: 'The Lean Startup' },
+  { id: 3, name: 'The Pragmatic Programmer' },
+];
+
+export default books;
